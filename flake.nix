b0f7e0296ff8a9a177745c097659a57fb18688b5
Aumy <46275902+AumyF@users.noreply.github.com>
2021-10-15 01:52:58 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A very basic flake";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      rec {
+        # packages = flake-utils.lib.flattenTree {
+        #   hello = pkgs.hello;
+        #   gitAndTools = pkgs.gitAndTools;
+        # };
+        # defaultPackage = packages.hello;
+        # apps.hello = flake-utils.lib.mkApp { drv = packages.hello; };
+        # defaultApp = apps.hello;
+        devShell = pkgs.callPackage ./shell.nix { };
+      }
+    );
+}
